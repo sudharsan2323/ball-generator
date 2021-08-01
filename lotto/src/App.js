bbd51d3661a9a9b56ae31ed60ff2799dfdb081b0
sudharsan2323 <87900468+sudharsan2323@users.noreply.github.com>
2021-08-01 21:48:23 +0700

    Add files via upload

---
+++ lotto/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Lottery from './Lottery';
+function App() {
+  return (
+    <div className="App">
+      <Lottery />
+    </div>
+  );
+}
+
+export default App;
---
+++ lotto/src/Ball.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './Ball.css'
+class Ball extends Component{
+    render() {
+        return (
+            <div className="ball">
+                {this.props.num}
+            </div>
+        )
+    }
+}
+export default Ball
